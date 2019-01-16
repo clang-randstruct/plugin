5873ae9623778658aed9ae340c35cde3d5fe6d4e
Connor Kuehl <cipkuehl@gmail.com>
2019-01-16 14:52:38 -0800

    clang-format

--- tests/randstruct_tests.cpp
+++ tests/randstruct_tests.cpp
@@ -1,11 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-int add(int a, int b) {
-    return a+b;
-}
+int add(int a, int b) { return a + b; }
 
-TEST_CASE( "Chaining math functions", "[functions]") {
-    REQUIRE( mult(2, 4) == 9 );
-    REQUIRE( add(1, 2) == 3 );
-}
+TEST_CASE("Chaining math functions", "[functions]") { REQUIRE(add(1, 2) == 3); }
