fb12c00e066ddd44608f165a9b57ee9786b07984
Connor Kuehl <cipkuehl@gmail.com>
2019-01-28 17:10:10 -0800

    Initial implementation of Buckets
    Co-authored-by: James Foster <jafosterja@gmail.com>

--- src/bucket.cpp
+++ src/bucket.cpp
@@ -1,9 +1,14 @@
+#include <algorithm>
+#include <random>
+
 #include "bucket.h"
 
 const size_t CACHE_LINE = 64;
 
 std::vector<FieldDecl *> Bucket::randomize() {
-    // TODO
+    // TODO use seed
+    auto rng = std::default_random_engine{};
+    std::shuffle(std::begin(fields), std::end(fields), rng);
     return fields;
 }
 
@@ -11,8 +16,17 @@ bool Bucket::canFit(size_t size) const {
     return this->size + size <= CACHE_LINE;
 }
 
-void Bucket::add(FieldDecl *field) {
+void Bucket::add(FieldDecl *field, size_t size) {
     fields.push_back(field);
+    this->size += size;
+}
+
+bool Bucket::isBitfieldRun() const {
+    return false;
+}
+
+bool Bucket::full() const {
+    return size == CACHE_LINE;
 }
 
 std::vector<FieldDecl *> BitfieldRun::randomize() {
@@ -20,5 +34,9 @@ std::vector<FieldDecl *> BitfieldRun::randomize() {
 }
 
 bool BitfieldRun::canFit(size_t size) const {
+    return true;
+}
+
+bool BitfieldRun::isBitfieldRun() const {
     return true;
 }
--- src/bucket.h
+++ src/bucket.h
@@ -11,7 +11,9 @@ class Bucket {
     public:
         virtual std::vector<FieldDecl *> randomize();
         virtual bool canFit(size_t size) const;
-        void add(FieldDecl *field);
+        void add(FieldDecl *field, size_t size);
+        virtual bool isBitfieldRun() const;
+        bool full() const;
 
     protected:
         size_t size;
@@ -22,4 +24,5 @@ class BitfieldRun : public Bucket {
     public:
         virtual std::vector<FieldDecl *> randomize() override;
         virtual bool canFit(size_t size) const override;
+        virtual bool isBitfieldRun() const override;
 };
